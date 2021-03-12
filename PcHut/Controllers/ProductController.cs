c5e278beddfdc338088995f4276b26f25b21915a
TajbiurShahriorRimon <tajbiurshahriorrimon@gmail.com>
2021-03-12 23:47:23 +0600

    Rimon's 1st push. Product list and add product done. However image work is not done yet

--- PcHut/Controllers/ProductController.cs
+++ PcHut/Controllers/ProductController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PcHut.Repository;
+using PcHut.Models;
+using System.IO;
 
 namespace PcHut.Controllers
 {
@@ -11,8 +14,37 @@ namespace PcHut.Controllers
         // GET: Product
         public ActionResult Index()
         {
+            ProductRepository products = new ProductRepository();
+            var allProducts = products.GetAll();
+            return View(allProducts);
+        }
 
+        [HttpGet]
+        public ActionResult Create()
+        {
+            CategoryRepository categoryList = new CategoryRepository();
+            ViewData["categories"] = categoryList.GetAll();
+
+            BrandRepository brandList = new BrandRepository();
+            ViewData["brands"] = brandList.GetAll();
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(product product)
+        {
+            /*string filePath = Server.MapPath("~/Image/");
+            string fileName = Path.GetFileName(product.image);
+
+            string fullFilePath = Path.Combine(filePath, fileName);
+            product.image.SaveAs(fullFilePath);
+            product.image = "~/Image/" + product.image.FileName;*/
+
+            ProductRepository addProduct = new ProductRepository();
+            product.status = 1;
+            addProduct.Insert(product);
+
+            return RedirectToAction("Index");
         }
     }
 }
