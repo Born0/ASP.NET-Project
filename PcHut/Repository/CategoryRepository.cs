aa628ad4fd34c6cbc1213b5009da3147ff3e490a
Born0 <bornoahmed2@gmail.com>
2021-03-12 21:22:28 +0600

    Initial commit

---
+++ PcHut/Repository/CategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using PcHut.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PcHut.Repository
+{
+    public class CategoryRepository : Repository<category>
+    {
+        public List<product> GetProducts(int id)
+        {
+            return null;
+        }
+    }
+}
